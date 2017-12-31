7b4192ca554d36be6c6abc4c3c3c171a2a8cc367
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2017-12-31 21:43:45 +0400

    Add files via upload

---
+++ files/gbDocs.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 29 22:52:29 2017
+
+@author: ARJ
+
+
+v 1.0.0
+
+1 variable declaration 
+    var x = 1
+    
+2 printing
+    2.1 single word
+    ====
+    print a
+    > a
+    
+    2.2 mathematical expression
+    engine : 
+        std eval - soon replaced by stacked Polish Notation engine4
+    ===
+    print (2+3-4)
+    > 1
+    
+    2.3 string
+    ===
+    print string a cat walks along the path
+    > a cat walks along the path
+    
+    2.4 variable
+    ===
+    print @x
+    > 1
+    
+3 linear graph
+    plot 1,2,3,4,5 x-time 23,34,56,78,45 y-letters
+    > 
+    
+4 minified for loop
+    for 3 times : print greenBerry
+    > greenBerry
+    > greenBerry
+    > greenBerry
+    
+5 comparisons
+    5.1 =
+    ===
+    if 2 = 2 : print leaf
+    > leaf
+    
+    if @x = 1 : print string x is 1
+    > x is 1
+    
+    if gfregg 4g4tg = g35g34554g : print 1
+    > 
+    
+    5.2 >
+    ===
+    if 2 > 3 : print tree
+    >
+    
+    if 3 > 2 : print berrry
+    > berrry
+    
+    if @x > 4 : print 1
+    >
+    
+    5.3 <
+    ===
+    if 2 < 3 : print tree
+    > tree
+    
+    if 3 < 2 : print berrry
+    > 
+    
+    if @x < 4 : print 1
+    > 1
+    
+6 functions
+    6.1 function declaration
+    ===
+    func vector : print string vector is fun
+    
+    6.2 function calling
+    ===
+    call vector
+    > vector is fun
+    
+7 classes
+    7.1 class declaration
+    class Man : power = 10 action walk : print walking...
+    
+    7.2 execute actions
+    make Man walk
+    > walking...
+    
+    7.3 see attributes / characteristics
+    see power of Man
+    > 10
+    
+    
+TODO
+- add eval in comparison
+- add vars in eval  
+    
+"""
+def x(**keyword_parameters):
+    print(keyword_parameters)
+    if ('name' in keyword_parameters):
+        if keyword_parameters['name'] == 1:
+            print(0)
+        else:
+            print(9)
+    print('leaving')
+    
+x(name = 1)
+x()
