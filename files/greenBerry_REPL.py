8dacf191b5adf2a7cc0b9666442c0c93a75a7427
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2017-12-31 23:10:44 +0400

    Add files via upload

---
+++ files/greenBerry_REPL.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 31 22:21:00 2017
+
+@author: ARJ
+"""
+from greenBerry import greenBerry_eval, S
+
+while 1:
+    x = input('---> ')
+    greenBerry_eval(x)
+    print()
