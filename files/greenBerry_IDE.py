5804e783dac6eaffa74435ef27f4efae249c5fd1
Tim Thuma <30806431+TimThuma@users.noreply.github.com>
2018-06-03 20:31:31 +0200

    Cleaning and upgrading
    Cleaned code, fixed highlighting bug, upgrades on running and saving gb code

--- files/greenBerry_IDE.py
+++ files/greenBerry_IDE.py
@@ -1,11 +1,10 @@
-from tkinter import * 
+from tkinter import *
+from tkinter import messagebox
 from tkinter import filedialog
 from greenBerry import greenBerry_eval
 
 file_dir = ""
-old_tags1 = [""]
-old_tags2 = []
-times_run = 0
+old_text = ""
 color1 = ["var", "print", "set", "debug", "plot"]
 color2 = ["string", "eval", "times", "action", "of", "to", "attribute",
           "bool"]
@@ -20,10 +19,12 @@ class Files(Frame):
 
         self.parent = parent        
         self.initUI()
+        parent.protocol("WM_DELETE_WINDOW", self.wclose)
 
     def initUI(self):
+        global old_text
 
-        self.parent.title("greenBerry IDE")
+        self.parent.title("greenBerry IDE - Untitlted")
         self.pack(fill=BOTH, expand=1)
 
         menubar = Menu(self.parent)
@@ -31,55 +32,77 @@ class Files(Frame):
 
         fileMenu = Menu(menubar)
         runMenu = Menu(menubar)
-        fileMenu.add_command(label="Save", command = self.save_command, accelerator="Ctrl+S")
+        fileMenu.add_command(label="Save", command = self.save_file, accelerator="Ctrl+S")
         fileMenu.add_command(label="Save As", command = self.save_as_command, accelerator="Ctrl+Shift+S")
-        fileMenu.add_command(label="Open", command = self.onOpen, accelerator="Ctrl+O")
+        fileMenu.add_command(label="Open", command = self.open_file, accelerator="Ctrl+O")
         menubar.add_cascade(label="File", menu=fileMenu)
 
         runMenu.add_command(label="Run", command = self.run_command, accelerator="F5")
-        menubar.add_cascade(label="Run", menu=runMenu, command = self.onOpen)        
+        menubar.add_cascade(label="Run", menu=runMenu, command = self.open_file)        
 
         self.bind_all("<F5>", self.run_command)
-        self.bind_all("<Control-o>", self.onOpen)
-        self.bind_all("<Control-s>", self.save_command)
+        self.bind_all("<Control-o>", self.open_file)
+        self.bind_all("<Control-s>", self.save_file)
         self.bind_all("<Control-S>", self.save_as_command)
-        self.bind_all("<Key>", self.color_text)
+        self.bind_all("<Key>", self.key_pressed)
 
         self.txt = Text(self)
         self.txt.pack(fill=BOTH, expand=1)
+        old_text = self.txt.get("1.0", END+"-1c")
+
+    def key_pressed(self, event=0):
+        self.color_text() #run syntax highlighting
         
-    def onOpen(self, event=0):
-        global file_dir
+        global old_text
+
+        a = self.parent.title()
+
+        if self.txt.get("1.0", END+"-1c") != old_text and a[0] != "*":
+            self.parent.title("*" + self.parent.title())
+            
+        elif self.txt.get("1.0", END+"-1c") == old_text and a[0] == "*":
+            self.parent.title(self.parent.title()[1:])
+                    
+    def open_file(self, event=0):
+        global file_dir, old_text
+
+        self.txt.delete(INSERT) #Ctrl+o causes a new line so we need to delete it
         
         ftypes = [("greenBerry files", "*.gb"), ("All files", "*")]
         file = filedialog.askopenfile(filetypes = ftypes)
 
         if file != None:
-            ile_dir = file.name
+            file_dir = file.name
             self.parent.title("greenBerry IDE" + " - " + file.name.replace("/", "\\"))
             self.txt.delete("1.0", END+"-1c")
-            text = self.readFile(file.name)
+            text = self.read_file(file.name)
             self.txt.insert(END, text)
-            self.color_text()
+            old_text = self.txt.get("1.0", END+"-1c")
+            self.key_pressed()
 
-    def readFile(self, filename):
+    def read_file(self, filename):
         f = open(filename, "r")
         text = f.read()
         return text
 
-    def save_command(self, event=0):
-        global file_dir
+    def save_file(self, event=0):
+        global file_dir, old_text
         
         try:
-            self.readFile(file_dir)
+            
+            self.read_file(file_dir)
             
             with open(file_dir, "w") as file:
                 file.write(self.txt.get("1.0", END+"-1c"))
+                file.close()
+                old_text = self.txt.get("1.0", END+"-1c")
+                self.key_pressed()
+
         except:
             self.save_as_command()
 
     def save_as_command(self, event=0):
-        global file_dir
+        global file_dir, old_text
         
         file = filedialog.asksaveasfile(mode="w", defaultextension=".gb", filetypes=(("greenBerry files", "*.gb"), ("All files", "*")))
         if file != None:
@@ -88,19 +111,43 @@ class Files(Frame):
             data = self.txt.get("1.0", END+"-1c")
             file.write(data)
             file.close()
+            old_text = self.txt.get("1.0", END+"-1c")
     
     def run_command(self, event=0):
+        global file_dir, old_text
+        
         x = self.txt.get("1.0", END+"-1c")
+
+        if x == old_text:
+            greenBerry_eval(self.read_file(file_dir))
+            print("="*30)
             
-        greenBerry_eval(x)
-        print("="*30)
+        elif messagebox.askokcancel("Save before run", "Your file must be saved before running.\nPress OK to save.") == True:
+            self.save_file()
+            try:
+                greenBerry_eval(self.read_file(file_dir))
+                print("="*30)
+            except:
+                self.run_command()
 
-    def color_text(self, event=0):
-        global old_tags1, old_tags2
-        global times_run
+    def wclose(self, event=0):
+        if self.parent.title()[0] == "*":
+            save = messagebox.askyesnocancel("Save file", "You have unsaved changes.\nDo you want to save before closing?")
 
-        times_run += 1
-                         
+            if save == True:
+                self.save_file()
+                if self.parent.title()[0] == "*":
+                    self.wclose()
+                else:
+                    root.destroy()
+
+            elif save == False:
+                root.destroy()
+        else:
+            root.destroy()
+        
+
+    def color_text(self, event=0):         
         file_text = self.txt.get("1.0", END+"-1c") + " "
         words = []
         line = 1
@@ -119,68 +166,37 @@ class Files(Frame):
             if char == " ":
                 words.append(word[:-1] + " : " + str(line) + "." + str(column))
                 word = ""
-                    
-        old_i = ["", ":", "1.0"]
-        old_tag = ""
-        if len(old_tags1) <= 2:
-            old_tags1 *= len(words)
-        tag_index = -1
+
+        for tag in self.txt.tag_names(): #deletes all tags so it can refresh them later
+            self.txt.tag_delete(tag)
 
         for i in words:
             
-            i1 = i
             i = i.split()
-            tag_index += 1
             
             if len(i) < 3:
                 i.insert(0, " ")
 
-            if len(old_i) < 3:
-                old_i.insert(0, " ")
-
             if i[0] in color1:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
                 self.txt.tag_add("color1", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), i[2])
                 self.txt.tag_config("color1", foreground="#9a1777")
-                old_tags2.append("color1")
                 
             elif i[0] in color2:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
                 self.txt.tag_add("color2", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), i[2])
                 self.txt.tag_config("color2", foreground="orange")
-                old_tags2.append("color2")
                 
             elif i[0] in color3:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
                 self.txt.tag_add("color3", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), i[2])
                 self.txt.tag_config("color3", foreground="#e60000")
-                old_tags2.append("color3")
 
             elif i[0][0] in color4:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
-                self.txt.tag_add("color4", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0].split("@")[1])))
+                self.txt.tag_add("color4", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])+1))
                 self.txt.tag_config("color4", foreground="orange")
-                old_tags2.append("color4")
 
             elif i[0] in color5:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
                 self.txt.tag_add("color5", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), i[2])
                 self.txt.tag_config("color5", foreground="#00cc00")
-                old_tags2.append("color5")
-
-            else:
-                self.txt.tag_remove(old_tags1[tag_index], old_i[2], str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])+len(i[0])))
-                self.txt.tag_add("color", str(i[2].split(".")[0]) + "." + str(int(i[2].split(".")[1])-len(i[0])), i[2])
-                self.txt.tag_config("color", foreground="black")
-                old_tags2.append("color")
                 
-            old_i = i1
-            old_i = old_i.split()
-
-        old_tags1 = old_tags2 + [""]
-        old_tags2 = []
-
-
 root = Tk()
 top = Frame(root)
 top.pack(fill=BOTH, expand=0)
