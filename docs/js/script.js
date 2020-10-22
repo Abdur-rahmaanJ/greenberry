bb86454020c93f01036e8f4a49f089d904f0f216
San411 <sandeshbhat2000@gmail.com>
2020-10-22 19:47:34 +0530

    A dark mode for the index page

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,3 +2,16 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+$( ".change" ).on("click", function() {
+  if( $( "body" ).hasClass( "dark" )) {
+                $( "body" ).removeClass( "dark" );
+              $( "div" ).addClass( "light" );
+              $( "span" ).removeClass( "black" );
+              document.getElementbyClassName("toggle").style.boxShadow = "none";
+              }
+  else{
+   $( "body" ).addClass( "dark" );
+   $( "span" ).addClass( "black" );
+ }
+});
