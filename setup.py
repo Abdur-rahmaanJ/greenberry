50fff59e7a5d77cc5055a9e976d55a9df6c9521a
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-08-24 15:49:03 +0400

    bump: 0.10.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="greenberry",  # Required
-    version="0.9.0",  # Required
+    version="0.10.0",  # Required
     description="lang compiler-theory interpreter",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
