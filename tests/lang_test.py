7ca41f5d8bedc2585c9b78f26db673d977c049a2
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-10-01 16:04:38 +0530

    updated lang_test

--- tests/lang_test.py
+++ tests/lang_test.py
@@ -1,16 +1,6 @@
-#
-# language tests
-#
-
-import sys
-
-sys.path.append("..")
-
 import unittest
-
-from utils import capture_eval_print
-
-from greenberry import greenberry_eval
+from greenberry.helpers import capture_print
+from greenberry.gb import greenberry_eval
 
 
 class GBLangTests(unittest.TestCase):
@@ -19,7 +9,7 @@ class GBLangTests(unittest.TestCase):
 
     def test_null(self):
         # check null input
-        x = ""
+        x = None
         try:
             greenberry_eval(x)
         except:
@@ -28,32 +18,32 @@ class GBLangTests(unittest.TestCase):
 
     def test_eval_add(self):
         x = "print eval 1+2"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "3")
 
     def test_eval_minus(self):
         x = "print eval 3-2"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "1")
 
     def test_eval_times(self):
         x = "print eval 3*2"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "6")
 
     def test_eval_divide(self):
         x = "print eval 10/2"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "5.0")
 
     def test_eval_mixed_ops(self):
         x = "print eval 1*3+10+3-2/2"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "15.0")
 
-    def test_eval_mixed_ops(self):
+    def test_eval_mixed_ops_with_parentheses(self):
         x = "print eval 1*3+10+3-2/2+(3+2)"
-        output = capture.eval_print(x)
+        output = capture_print(greenberry_eval, x)
         self.assertEqual(output, "20.0")
 
 
