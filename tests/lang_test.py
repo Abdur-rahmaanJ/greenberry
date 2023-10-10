ed5c6e929f9340114e920980c30db77dcd91e908
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2023-10-10 11:07:25 +0400

    Revert "Refactoring and Enhanced capture_print Function "

--- tests/lang_test.py
+++ tests/lang_test.py
@@ -1,6 +1,16 @@
+#
+# language tests
+#
+
+import sys
+
+sys.path.append("..")
+
 import unittest
-from greenberry.helpers import capture_print
-from greenberry.gb import greenberry_eval
+
+from utils import capture_eval_print
+
+from greenberry import greenberry_eval
 
 
 class GBLangTests(unittest.TestCase):
@@ -9,7 +19,7 @@ class GBLangTests(unittest.TestCase):
 
     def test_null(self):
         # check null input
-        x = None
+        x = ""
         try:
             greenberry_eval(x)
         except:
@@ -18,32 +28,32 @@ class GBLangTests(unittest.TestCase):
 
     def test_eval_add(self):
         x = "print eval 1+2"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "3")
 
     def test_eval_minus(self):
         x = "print eval 3-2"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "1")
 
     def test_eval_times(self):
         x = "print eval 3*2"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "6")
 
     def test_eval_divide(self):
         x = "print eval 10/2"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "5.0")
 
     def test_eval_mixed_ops(self):
         x = "print eval 1*3+10+3-2/2"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "15.0")
 
-    def test_eval_mixed_ops_with_parentheses(self):
+    def test_eval_mixed_ops(self):
         x = "print eval 1*3+10+3-2/2+(3+2)"
-        output = capture_print(greenberry_eval, x)
+        output = capture.eval_print(x)
         self.assertEqual(output, "20.0")
 
 
