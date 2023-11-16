4661ddfe9e55bd13d0a7b7c13f6dced142fa9266
0x32 <95779166+0x32767@users.noreply.github.com>
2023-11-16 21:26:48 +0000

    improved CLI
    Used argparse to create an improved CLI, now supports starting the repl from the greenberry CLI.

--- src/greenberry/repl.py
+++ src/greenberry/repl.py
@@ -5,24 +5,36 @@ Created on Sun Dec 31 22:21:00 2017
 """
 from greenberry.gb import greenberry_eval
 
-print(
-    """
-      ---greenberry(c)---
-      welcome to the .gb REPL
-      ---greenberry(c)---
-      """
-)
-isSessionOn = 1
-while isSessionOn == 1:
-    x = input("---> ")
-    greenberry_eval(x)
-    if x == "berry exit":
-        isSessionOn = 0
-    print()
-print(
-    """
-      ---greenberry(c)---
-      .gb REPL exited. see you soon _ _
-      ---greenberry(c)---
-      """
-)
+
+def run_repl():
+    print(
+        """
+  ---greenberry(c)---
+  welcome to the .gb REPL
+  ---greenberry(c)---
+  """
+    )
+    try:
+        while True:
+            x = input("---> ")
+            if x == "berry exit":
+                break
+
+            greenberry_eval(x)
+
+            print()
+
+    except KeyboardInterrupt:
+        pass
+
+    print(
+        """
+  ---greenberry(c)---
+  .gb REPL exited. see you soon _ _
+  ---greenberry(c)---
+  """
+    )
+
+
+if __name__ == "__main__":
+    run_repl()
